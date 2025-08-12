2b16e8e504111e319fa93b22a9cce15ed99b3712
rodsantis <desantisrod@gmail.com>
2025-08-12 05:54:29 +0200

    Layout page create, worked on index page and set the structure in place. It is not communicating yet with backend

--- core/urls.py
+++ core/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from punch.views import *
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', index),
+    path('punch/', include("punch.urls")),
 ]
--- punch/views.py
+++ punch/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    """
+        The idea here is that the U when redirected to index, it will be first checked if:
+            they are logged in;
+            they are management role or not;
+            they are the admin managing the shifts of employees;
+
+        based on that a different path of return will be shown,
+        as per right now it will be render only the employee path
+    """
+    return render(request, 'employee/index.html')
